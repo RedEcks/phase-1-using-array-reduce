ce6ca13c3142383bf52d18b5f83950078d6c9602
Xavier Dunkley <xavierxv18@gmail.com>
2022-02-21 08:18:11 -0500

    commit tree 61b060d6b1614080a21267b9435d8c9f5b6a1fe9

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteriesInitial = 0;
+const totalBatteries = batteryBatches.reduce(
+    (begin,end)=>begin+end,totalBatteriesInitial);
+
+    console.log(totalBatteries);
